4b44afc50f6fec37831973b6a035b07013418410
TBExp <info@teambuildingexperience.it>
2026-08-11 18:06:22 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://ifuxoakybyjzzjmfaqrv.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmdXhvYWt5YnlqenpqbWZhcXJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY0NTY2NzgsImV4cCI6MjEwMjAzMjY3OH0.pHDchxZmyzM1YS_O-w0n2GXIKQbUFs_4aWvZXto6Nww",
+  APP_NAME: "ROAD CONTROL"
+};
